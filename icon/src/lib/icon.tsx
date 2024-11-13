3365d37b74c25225caa887e68a6bd4f6723be308
nartc <nartc7789@gmail.com>
2024-11-13 17:13:25 -0600

    done

--- icon/src/lib/icon.tsx
+++ icon/src/lib/icon.tsx
@@ -3,7 +3,7 @@ import styles from './icon.module.css';
 export function Icon() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Icon!</h1>
+      <h1>Welcome to Icon!!</h1>
     </div>
   );
 }
